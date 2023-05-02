03a9980829020173f580557f2666970d3dd0a28c
user404e <131599479+user404e@users.noreply.github.com>
2023-05-02 11:24:06 +0000

    Update Solution.java

--- JAVA/Java Stdin And Stdout 1/Solution.java
+++ JAVA/Java Stdin And Stdout 1/Solution.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 public class Solution {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();                
+        int c = scan.nextInt();
         
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
     }
 }
